364f93536a24d754728c05c77853c8b9005572f6
Lucas Gabriel <lucas1020111940@hotmail.com>
2023-02-28 22:23:59 -0300

    first commit, create project

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="todoapp">
+    <header class="header">
+      <h1 class="todo-title">Tarefas</h1>
+      <input-task></input-task>
+    </header>
+  </section>
+</template>
+
+<script>
+import InputTask from './components/InputTask.vue'
+
+export default {
+  name: 'App',
+  components: {
+    InputTask
+  }
+}
+</script>
+
+<style>
+* {
+  padding: 0%;
+  margin: 0%;
+}
+
+.todoapp {
+  background-color: #1f6afc;
+  margin: 5%;
+  height: 70%;
+}
+
+.todo-title {
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  text-align: center;
+  color: black;
+  font-weight: bold;
+}
+
+</style>
---
+++ src/components/InputTask.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input class="new-todo" @keyup.enter="addTask" placeholder="O que precisa ser feito?">
+  </div>
+</template>
+
+<script>
+import { Task } from '../models/Task';
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods:{
+
+    addTask($event){
+      let task = new Task()
+      let value = $event.target.value
+      task.completed = false
+      task.title = value
+      console.log(task)
+    }
+  }
+}
+</script>
+
+<style>
+
+.new-todo{
+  border-radius: 5px;
+  padding: 1%;
+  margin: 2%;
+  width: 30%;
+  margin-inline-start: 35%;
+  text-align: center;
+}
+
+</style>
